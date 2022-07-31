3ade77d650ec4e328c2873f97f0079f5b7d4c513
Mitchell Gruner <mgruner@csus.edu>
2022-07-31 12:40:55 -0500

    still lot of work to do, try to go to Quiz page

--- src/Start.js
+++ src/Start.js
@@ -1,32 +1,53 @@
-import React, {Component} from 'react';
+import * as React from 'react';
+import Quiz from './Quiz';
 import './Start.css';
 import './Quiz.css';
 
-class Start extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            numQuestions: []
-        }
+const Start = () => {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         difficulty: [],
+    //         numQuestions: [],
+    //     }
+    //     this.handleSubmit = this.handleSubmit.bind(this);
+    //     this.handleChangeDifficulty = this.handleChangeDifficulty.bind(this);
+    //     this.handleChangeQuestions = this.handleChangeQuestions.bind(this);
+    // }
+    const [value, setValue] = React.useState("Easy");
+
+    const handleChange = (evt) => {
+        setValue(evt.target.value);
+        <Quiz difficulty={evt.target.value} />
     }
 
-    handleSubmit (evt) {
-
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        <Quiz />
     }
 
-    render() {
-        return (
-            <div>
-                <div className="Start-box">
-                    <h1 className="sidebar-title"><span>Quiz</span> List</h1>
-                    <i className="em em-brain"></i>
-                    <form onSubmit={this.handleSubmit}>
+    const gameDifficulty = [
+        {label: "Easy", value: "Easy"},
+        {label: "Medium", value: "Medium"},
+        {label: "Hard", value: "Hard"}
+    ]
 
-                    </form>
-                </div>
+    return (
+        <div>
+            <div className="Start-box">
+                <h1 className="sidebar-title"><span>Quiz</span> List</h1>
+                <i className="em em-brain"></i>
+                <form onSubmit={handleSubmit} className="Start-form">
+                    <select value={value} onChange={handleChange}>
+                        {gameDifficulty.map((dif, idx) => (
+                            <option key={idx} value={dif.value}>{dif.label}</option>
+                        ))}
+                    </select>
+                    <button>Submit!</button>
+                </form>
             </div>
-        )
-    }
+        </div>
+    );
 }
 
 export default Start;
