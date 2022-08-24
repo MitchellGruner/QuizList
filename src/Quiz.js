6c96bcaaf6039ecdc7fec034bb3cebe79721136c
Mitchell Gruner <mitchell.gruner@electronicexpress.com>
2022-08-24 12:25:02 -0500

    have accordion element delete if answer is correct

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,99 +1,117 @@
-import React, {Component} from 'react';
-import Answers from './Answers';
-import {Accordion} from "react-bootstrap";
-import {CSSTransition, TransitionGroup} from 'react-transition-group';
-import './Quiz.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import Answers from "./Answers";
+import { Accordion } from "react-bootstrap";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import "./Quiz.css";
+import uuid from "react-uuid";
+import axios from "axios";
 
 class Quiz extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            accordion: [],
-            numOfQuestions: 15,
-            questionsArr: JSON.parse(window.localStorage.getItem("questionsArr") || "[]")
-        };
-        this.getQuestions = this.getQuestions.bind(this);
-        this.create = this.create.bind(this);
-        this.handleDeletion = this.handleDeletion.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			numOfQuestions: 15,
+			questionsArr: JSON.parse(
+				window.localStorage.getItem("questionsArr") || "[]"
+			),
+		};
+		this.getQuestions = this.getQuestions.bind(this);
+		this.create = this.create.bind(this);
+		this.handleDeletion = this.handleDeletion.bind(this);
+	}
 
-    componentDidMount() {
-        if (this.state.questionsArr.length === 0) {
-            this.getQuestions();
-        }
-    }
+	componentDidMount() {
+		if (this.state.questionsArr.length === 0) {
+			this.getQuestions();
+		}
+	}
 
-    async getQuestions() {
-        let arr = [];
-        let i = 0;
+	async getQuestions() {
+		let arr = [];
+		let i = 0;
 
-        let res = await axios.get(`https://opentdb.com/api.php?amount=${this.state.numOfQuestions}&difficulty=${this.props.difficulty}&type=multiple`);
+		let res = await axios.get(
+			`https://opentdb.com/api.php?amount=${this.state.numOfQuestions}&difficulty=${this.props.difficulty}&type=multiple`
+		);
 
-        console.log(res);
-        
-        while (i < this.state.numOfQuestions) {
-            arr.push(res.data.results[i]);
-            i++;
-        }
+		console.log(res);
 
-        this.setState({
-            questionsArr: arr
-        });
-        
-        window.localStorage.setItem(
-            "questionsArr",
-            JSON.stringify(arr)
-        )
+		while (i < this.state.numOfQuestions) {
+			arr.push(res.data.results[i]);
+			i++;
+		}
 
-        window.location.reload();
-    }
+		this.setState({
+			questionsArr: arr,
+		});
 
-    create (newAccordion) {
-        this.setState({
-            accordion: [...this.state.accordion, newAccordion]
-        });
-    }
+		window.localStorage.setItem("questionsArr", JSON.stringify(arr));
 
-    handleDeletion (id) {
-        this.setState({
-            accordion: this.state.accordion.filter(a => a.id !== id)
-        });
-    }
+		window.location.reload();
+	}
 
-    render() {
-        return (
-            <div className="Quiz">
-                <div className="Quiz-sidebar">
-                    <h1 className="sidebar-title"><span>Quiz</span> List</h1>
-                    <i className="em em-brain"></i>
-                    <button onClick={this.getQuestions}>Remove</button>
-                </div>
-                <div className="Quiz-quizlist">
-                    {this.props.difficulty}
-                    <TransitionGroup>
-                        {this.state.questionsArr.map((question, idx) => (
-                            <CSSTransition key={idx} timeout={700} className="Quiz-transition">
-                                <Accordion>
-                                    <Accordion.Item eventKey={idx}>
-                                        <Accordion.Header className="Quiz-header">{question.category}</Accordion.Header>
-                                        <Accordion.Body>
-                                        {question.question}
-                                        <Answers key={idx} 
-                                            incorrect={question.incorrect_answers} 
-                                            correct={question.correct_answer} 
-                                            removeAccordion={this.handleDeletion}
-                                        />
-                                        </Accordion.Body>
-                                    </Accordion.Item>
-                                </Accordion>
-                            </CSSTransition>
-                        ))}
-                    </TransitionGroup>
-                </div>
-            </div>
-        )
-    }
+	create(newAccordion) {
+		this.setState({
+			accordion: [...this.state.accordion, newAccordion],
+		});
+	}
+
+	handleDeletion(answer) {
+		this.setState({
+			questionsArr: this.state.questionsArr.filter((obj) => {
+				return obj.incorrect_answers !== answer;
+			}),
+		});
+	}
+
+	render() {
+		return (
+			<div className="Quiz">
+				<div className="Quiz-sidebar">
+					<h1 className="sidebar-title">
+						<span>Quiz</span> List
+					</h1>
+					<i className="em em-brain"></i>
+					<button onClick={this.getQuestions}>Remove</button>
+				</div>
+				<div className="Quiz-quizlist">
+					{this.props.difficulty}
+					<TransitionGroup>
+						{this.state.questionsArr.map((question) => (
+							<CSSTransition
+								key={uuid()}
+								timeout={700}
+								className="Quiz-transition"
+							>
+								<Accordion>
+									<Accordion.Item eventKey={uuid()}>
+										<Accordion.Header className="Quiz-header">
+											{question.category}
+										</Accordion.Header>
+										<Accordion.Body>
+											{question.question}
+											<Answers
+												idKey={uuid()}
+												incorrect={
+													question.incorrect_answers
+												}
+												correct={
+													question.correct_answer
+												}
+												removeAccordion={
+													this.handleDeletion
+												}
+											/>
+										</Accordion.Body>
+									</Accordion.Item>
+								</Accordion>
+							</CSSTransition>
+						))}
+					</TransitionGroup>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Quiz;
