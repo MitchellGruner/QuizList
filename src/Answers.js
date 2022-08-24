637a6536cff3da94c5f6a6986c9364de7270bcae
Mitchell Gruner <mitchell.gruner@electronicexpress.com>
2022-08-24 12:26:08 -0500

    update handleClick function for removing element

--- src/Answers.js
+++ src/Answers.js
@@ -1,75 +1,84 @@
-import React, {Component} from 'react';
-import './Answers.css';
+import React, { Component } from "react";
+import "./Answers.css";
 
 class Answers extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            answersArr: [],
-            correctArr: []
-        }
-        this.handleClick = this.handleClick.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			answersArr: [],
+			correctArr: [],
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
 
-    async componentDidMount() {
-        let arr1 = [];
-        let arr2 = [];
+	async componentDidMount() {
+		let arr1 = [];
+		let arr2 = [];
 
-        for (let i = 0; i < this.props.incorrect.length; i++) {
-            arr1.push(this.props.incorrect[i]);
-        }
+		for (let i = 0; i < this.props.incorrect.length; i++) {
+			arr1.push(this.props.incorrect[i]);
+		}
 
-        arr1.push(this.props.correct);
-        arr2.push(this.props.correct);
+		arr1.push(this.props.correct);
+		arr2.push(this.props.correct);
 
-        function shuffleArray(array) {
-            for (let i = array.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [array[i], array[j]] = [array[j], array[i]];
-            }
-        }
+		function shuffleArray(array) {
+			for (let i = array.length - 1; i > 0; i--) {
+				const j = Math.floor(Math.random() * (i + 1));
+				[array[i], array[j]] = [array[j], array[i]];
+			}
+		}
 
-        shuffleArray(arr1);
+		shuffleArray(arr1);
 
-        this.setState({
-            answersArr: arr1,
-            correctArr: arr2
-        });
-    }
+		this.setState({
+			answersArr: arr1,
+			correctArr: arr2,
+		});
+	}
 
-    handleClick (arg) {
-        if (this.state.correctArr.includes(this.state.answersArr[arg])) {
-            console.log("correct!");
-            this.props.removeAccordion(this.props.key);
-        } else {
-            console.log("incorrect!");
-        }
-    }
+	handleClick(arg) {
+		if (this.state.correctArr.includes(this.state.answersArr[arg])) {
+			this.props.removeAccordion(this.props.incorrect);
+		}
+	}
 
-    render() {
-        return (
-            <div className="Answers">
-                <div className="Answers-container">
-                    <div className="row flex-row">
-                        <button onClick={() => this.handleClick(0)} className="Answers-btn col-md-6">
-                            {this.state.answersArr[0]}
-                        </button>
-                        <button onClick={() => this.handleClick(1)} className="Answers-btn col-md-6">
-                            {this.state.answersArr[1]}
-                        </button>
-                    </div>
-                    <div className="row flex-row">
-                        <button onClick={() => this.handleClick(2)} className="Answers-btn col-md-6">
-                            {this.state.answersArr[2]}
-                        </button>
-                        <button onClick={() => this.handleClick(3)} className="Answers-btn col-md-6">
-                            {this.state.answersArr[3]}
-                        </button>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="Answers">
+				<div className="Answers-container">
+					<div className="row flex-row">
+						<button
+							onClick={() => this.handleClick(0)}
+							className="Answers-btn col-md-6"
+						>
+							{this.state.answersArr[0]}
+						</button>
+						<button
+							onClick={() => this.handleClick(1)}
+							className="Answers-btn col-md-6"
+						>
+							{this.state.answersArr[1]}
+						</button>
+					</div>
+					<div className="row flex-row">
+						<button
+							onClick={() => this.handleClick(2)}
+							className="Answers-btn col-md-6"
+						>
+							{this.state.answersArr[2]}
+						</button>
+						<button
+							onClick={() => this.handleClick(3)}
+							className="Answers-btn col-md-6"
+						>
+							{this.state.answersArr[3]}
+						</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Answers;
