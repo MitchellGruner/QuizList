c71c76b11f40b39e9e061e4ba67b3efefdaf76fc
Mitchell Gruner <mitchell.gruner@electronicexpress.com>
2022-08-25 09:53:07 -0500

    initial commit

---
+++ src/Timer.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+
+const Timer = () => {
+	const initialTimer = localStorage.getItem("timer") ?? 100;
+	const timeoutId = React.useRef(null);
+	const [timer, setTimer] = React.useState(initialTimer);
+
+	const countTimer = React.useCallback(() => {
+		if (timer <= 0) {
+			localStorage.clear("timer");
+		} else {
+			setTimer(timer - 1);
+			localStorage.setItem("timer", timer);
+		}
+	}, [timer]);
+
+	React.useEffect(() => {
+		timeoutId.current = window.setTimeout(countTimer, 1000);
+
+		return () => window.clearTimeout(timeoutId.current);
+	}, [timer, countTimer]);
+
+	return <div align="center">{timer}</div>;
+};
+
+export default Timer;
+
+// heavily referenced this URL for the code:
+// https://stackoverflow.com/questions/68134405/react-component-using-hooks-sync-state-count-down-time-with-local-storage
