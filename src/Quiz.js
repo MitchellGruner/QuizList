7ae2f0cc0f74fdd488c504179adfdaed700501af
Mitchell Gruner <mit.gruner@gmail.com>
2022-11-20 11:40:34 -0600

    filter html encodings out of incorrect answers

--- src/Quiz.js
+++ src/Quiz.js
@@ -48,10 +48,9 @@ class Quiz extends Component {
 			i++;
 		}
 
+		/* for questions */
 		for (let j = 0; j < arr.length; j++) {
 			for (let i = 0; i < arr.length; i++) {
-
-				/* for questions */
 				if (arr[i].question.includes("&#039;")) {
 					arr[i].question = arr[i].question.replace("&#039;", "'");
 				}
@@ -79,7 +78,397 @@ class Quiz extends Component {
 				if (arr[i].question.includes("&rsquo")) {
 					arr[i].question = arr[i].question.replace("&rsquo", "'");
 				}
+
+				if (arr[i].question.includes("&ouml;")) {
+					arr[i].question = arr[i].question.replace("&ouml;", "ö");
+				}
+
+				if (arr[i].question.includes("&Ouml;")) {
+					arr[i].question = arr[i].question.replace("&Ouml;", "Ö");
+				}
+				
+				if (arr[i].question.includes("&aacute;")) {
+					arr[i].question = arr[i].question.replace("&aacute;", "á")
+				}
+
+				if (arr[i].question.includes("&Aacute;")) {
+					arr[i].question = arr[i].question.replace("&Aacute;", "Á")
+				}
+
+				if (arr[i].question.includes("&eacute;")) {
+					arr[i].question = arr[i].question.replace("&eacute;", "é")
+				}
+
+				if (arr[i].question.includes("&Eacute;")) {
+					arr[i].question = arr[i].question.replace("&Eacute;", "É");
+				}
+
+				if (arr[i].question.includes("iacute;")) {
+					arr[i].question = arr[i].question.replace("&iacute;", "í")
+				}
+
+				if (arr[i].question.includes("Iacute;")) {
+					arr[i].question = arr[i].question.replace("&Iacute;", "Í");
+				}
+
+				if (arr[i].question.includes("&oacute;")) {
+					arr[i].question = arr[i].question.replace("&oacute;", "ó")
+				}
+
+				if (arr[i].question.includes("Oacute;")) {
+					arr[i].question = arr[i].question.replace("&Oacute;", "Ó");
+				}
+
+				if (arr[i].question.includes("&uacute;")) {
+					arr[i].question = arr[i].question.replace("&uacute;", "ú")
+				}
+
+				if (arr[i].question.includes("Uacute;")) {
+					arr[i].question = arr[i].question.replace("&Uacute;", "Ú");
+				}
 			}
+		}
+
+		/* for correct answers */
+		for (let i = 0; i < arr.length; i++) {
+			for (let j = 0; j < arr.length; j++) {
+				if (arr[i].correct_answer.includes("&#039;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&#039;", "'");
+				}
+	
+				if (arr[i].correct_answer.includes("&quot;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&quot;", "'");
+				}
+				
+				if (arr[i].correct_answer.includes("&amp;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&amp;", "&");
+				}
+
+				if (arr[i].correct_answer.includes("&Uuml;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&Uuml;", "Ü");
+				}
+
+				if (arr[i].correct_answer.includes("&uuml;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&Uuml;", "ü");
+				}
+
+				if (arr[i].correct_answer.includes("&shy;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&shy;", "-");
+				}
+
+				if (arr[i].correct_answer.includes("&rsquo")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&rsquo", "'");
+				}
+
+				if (arr[i].correct_answer.includes("&ouml;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&ouml;", "ö");
+				}
+
+				if (arr[i].correct_answer.includes("&Ouml;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&Ouml;", "Ö");
+				}
+
+				if (arr[i].correct_answer.includes("&aacute;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&aacute;", "á")
+				}
+
+				if (arr[i].correct_answer.includes("&Aacute;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&Aacute;", "Á")
+				}
+
+				if (arr[i].correct_answer.includes("&eacute;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&eacute;", "é")
+				}
+
+				if (arr[i].correct_answer.includes("&Eacute;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&Eacute;", "É");
+				}
+
+				if (arr[i].correct_answer.includes("iacute;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&iacute;", "í")
+				}
+
+				if (arr[i].correct_answer.includes("Iacute;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&Iacute;", "Í");
+				}
+
+				if (arr[i].correct_answer.includes("&oacute;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&oacute;", "ó")
+				}
+
+				if (arr[i].correct_answer.includes("Oacute;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&Oacute;", "Ó");
+				}
+
+				if (arr[i].correct_answer.includes("&uacute;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&uacute;", "ú")
+				}
+
+				if (arr[i].correct_answer.includes("Uacute;")) {
+					arr[i].correct_answer = arr[i].correct_answer.replace("&Uacute;", "Ú");
+				}
+			}
+		}
+
+		/* for incorrect answers */
+		for (let i = 0; i < arr.length; i++) {
+			for (let j = 0; j < arr.length; j++) {
+				if (arr[i].incorrect_answers[0].includes("&#039;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&#039;", "'");
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&#039;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&#039;", "'");
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&#039;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&#039;", "'");
+				}
+
+				if (arr[i].incorrect_answers[0].includes("&quot;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&quot;", "'");
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&quot;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&quot;", "'");
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&quot;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&quot;", "'");
+				}
+				
+				if (arr[i].incorrect_answers[0].includes("&amp;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&amp;", "&");
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&amp;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&amp;", "&");
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&amp;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&amp;", "&");
+				}
+
+				if (arr[i].incorrect_answers[0].includes("&Uuml;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&Uuml;", "Ü");
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&Uuml;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&Uuml;", "Ü");
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&Uuml;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&Uuml;", "Ü");
+				}
+
+				if (arr[i].incorrect_answers[0].includes("&uuml;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&Uuml;", "ü");
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&uuml;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&Uuml;", "ü");
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&uuml;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&Uuml;", "ü");
+				}
+
+				if (arr[i].incorrect_answers[0].includes("&shy;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&shy;", "-");
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&shy;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&shy;", "-");
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&shy;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&shy;", "-");
+				}
+
+				if (arr[i].incorrect_answers[0].includes("&rsquo")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&rsquo", "'");
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&rsquo")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&rsquo", "'");
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&rsquo")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&rsquo", "'");
+				}
+
+				if (arr[i].incorrect_answers[0].includes("&ouml;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&ouml;", "ö");
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&ouml;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&ouml;", "ö");
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&ouml;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&ouml;", "ö");
+				}
+
+				if (arr[i].incorrect_answers[0].includes("&Ouml;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&Ouml;", "Ö");
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&Ouml;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&Ouml;", "Ö");
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&Ouml;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&Ouml;", "Ö");
+				}
+
+				if (arr[i].incorrect_answers[0].includes("&aacute;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&aacute;", "á")
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&aacute;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&aacute;", "á")
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&aacute;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&aacute;", "á")
+				}
+
+				if (arr[i].incorrect_answers[0].includes("&Aacute;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&Aacute;", "Á")
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&Aacute;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&Aacute;", "Á")
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&Aacute;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&Aacute;", "Á")
+				}
+
+				if (arr[i].incorrect_answers[0].includes("&eacute;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&eacute;", "é")
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&eacute;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&eacute;", "é")
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&eacute;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&eacute;", "é")
+				}
+
+				if (arr[i].incorrect_answers[0].includes("&Eacute;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&Eacute;", "É");
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&Eacute;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&Eacute;", "É");
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&Eacute;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&Eacute;", "É");
+				}
+
+				if (arr[i].incorrect_answers[0].includes("iacute;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&iacute;", "í")
+				}
+
+				if (arr[i].incorrect_answers[1].includes("iacute;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&iacute;", "í")
+				}
+
+				if (arr[i].incorrect_answers[2].includes("iacute;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&iacute;", "í")
+				}
+
+				if (arr[i].incorrect_answers[0].includes("Iacute;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&Iacute;", "Í");
+				}
+
+				if (arr[i].incorrect_answers[1].includes("Iacute;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&Iacute;", "Í");
+				}
+
+				if (arr[i].incorrect_answers[2].includes("Iacute;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&Iacute;", "Í");
+				}
+
+				if (arr[i].incorrect_answers[0].includes("&oacute;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&oacute;", "ó")
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&oacute;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&oacute;", "ó")
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&oacute;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&oacute;", "ó")
+				}
+
+				if (arr[i].incorrect_answers[0].includes("Oacute;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&Oacute;", "Ó");
+				}
+
+				if (arr[i].incorrect_answers[1].includes("Oacute;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&Oacute;", "Ó");
+				}
+
+				if (arr[i].incorrect_answers[2].includes("Oacute;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&Oacute;", "Ó");
+				}
+
+				if (arr[i].incorrect_answers[0].includes("&uacute;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&uacute;", "ú")
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&uacute;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&uacute;", "ú")
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&uacute;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&uacute;", "ú")
+				}
+
+				if (arr[i].incorrect_answers[0].includes("Uacute;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&Uacute;", "Ú");
+				}
+
+				if (arr[i].incorrect_answers[1].includes("Uacute;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&Uacute;", "Ú");
+				}
+
+				if (arr[i].incorrect_answers[2].includes("Uacute;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&Uacute;", "Ú");
+				}
+
+				if (arr[i].incorrect_answers[0].includes("&lrm;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&lrm;", "");
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&lrm;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&lrm;", "");
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&lrm;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&lrm;", "");
+				}
+
+				if (arr[i].incorrect_answers[0].includes("&rlm;")) {
+					arr[i].incorrect_answers[0] = arr[i].incorrect_answers[0].replace("&rlm;", "");
+				}
+
+				if (arr[i].incorrect_answers[1].includes("&rlm;")) {
+					arr[i].incorrect_answers[1] = arr[i].incorrect_answers[1].replace("&rlm;", "");
+				}
+
+				if (arr[i].incorrect_answers[2].includes("&rlm;")) {
+					arr[i].incorrect_answers[2] = arr[i].incorrect_answers[2].replace("&rlm;", "");
+				}
+			}
+		}
+
+		for (let i = 0; i < arr.length; i++) {
+			console.log(arr[i].incorrect_answers);
 		}
 
 		this.setState({
@@ -132,7 +521,7 @@ class Quiz extends Component {
 
 	wrongScore() {
 		document.getElementById("initialState").style.display = "none";
-		document.getElementById("wrongguess").style.display = "block";
+		document.getElementById("wrongguess").style.display = "flex";
 	}
 
 	getEmoji() {
@@ -215,7 +604,8 @@ class Quiz extends Component {
 					</TransitionGroup>
 				</div>
 				<div id="wrongguess" className="Quiz-quizlist-wrong">
-					<h1>Game Over!</h1>
+					<span className="quizlist-wrong-header">Game Over!</span><br />
+					<p className="quizlist-wrong-paragraph">Your final score was {this.state.score}</p>
 					<button
 						onClick={() => {this.getQuestions(); this.refresh();}}
 						className="Quiz-newGame Quiz-parallelogram"
@@ -224,9 +614,6 @@ class Quiz extends Component {
 						</div>
 					</button>
 				</div>
-				{/* <div className="Quiz-timer">
-					<Timer />
-				</div> */}
 			</div>
 		);
 	}
